97c3dadd132bacd95d9ae00cb0463a5803337b43
Luca Cireddu <luca@lucacireddu.it>
2013-08-14 20:00:33 +0200

    First infos acquired

---
+++ includes/head.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * PHP Site for Icecast MySQL Stats
+ * Copyright (C) 2013  Luca Cireddu
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * You can also find an on-line copy of this license at:
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Created and mantained by: Luca Cireddu
+ *                           sardylan@gmail.com
+ *                           http://www.lucacireddu.it
+ *
+ */
+
+define("MYSQL_SERVER", "127.0.0.1");
+define("MYSQL_PORT", 3306);
+define("MYSQL_USER", "smlgr");
+define("MYSQL_PASSWORD", "smlgr");
+define("MYSQL_DB", "smlgr");
+
+define("MYSQL_TABLE", "invdata");
+
+$sql_conn = new mysqli(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB);
+
+if($sql_conn->connect_error)
+    die("Connect Error (" . $mysqli->connect_errno . "): " . $mysqli->connect_error);
+
+$now = time();
+$today = date("Y-m-d", $now);
+
+?>
---
+++ index.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * PHP Site for Icecast MySQL Stats
+ * Copyright (C) 2013  Luca Cireddu
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * You can also find an on-line copy of this license at:
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Created and mantained by: Luca Cireddu
+ *                           sardylan@gmail.com
+ *                           http://www.lucacireddu.it
+ *
+ */
+
+
+$page_path = "";
+$cwd = getcwd();
+$site_root = substr(getcwd(), 0, strlen($cwd) - strlen($page_path));
+
+require_once($site_root . "/includes/head.php");
+
+$sql_interval = "whenquery > '{$today} 00:00:00' AND whenquery < '{$today} 23:59:59'";
+
+$sql_query = "SELECT whenquery AS start FROM " . MYSQL_TABLE . " WHERE {$sql_interval} ORDER BY whenquery ASC LIMIT 0, 1";
+
+error_log($sql_query);
+
+if($sql_result = $sql_conn->query($sql_query)) {
+    if($sql_result->num_rows > 0) {
+        if($sql_data = $sql_result->fetch_array(MYSQLI_ASSOC)) {
+            $inv_start = $sql_data["start"];
+        }
+    }
+}
+
+$sql_query = "SELECT whenquery AS stop FROM " . MYSQL_TABLE . " WHERE {$sql_interval} ORDER BY whenquery DESC LIMIT 0, 1";
+
+error_log($sql_query);
+
+if($sql_result = $sql_conn->query($sql_query)) {
+    if($sql_result->num_rows > 0) {
+        if($sql_data = $sql_result->fetch_array(MYSQLI_ASSOC)) {
+            $inv_stop = $sql_data["stop"];
+        }
+    }
+}
+
+?>
+<html>
+
+    <head>
+        <title>SolarMax Inverters Stats</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    </head>
+
+    <body>
+        <div id="page">
+            <h1>Today activity</h1>
+            <h2>Start working at <?php echo $inv_start; ?> and stop working at <?php echo $inv_stop; ?></h2>
+            <img src="images.php" alt="Graph" title="Graph" />
+        </div>
+    </body>
+
+</html>
